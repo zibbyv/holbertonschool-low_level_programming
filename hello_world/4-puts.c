bec8a9be178ddb6a5118b884b252cfb0138300db
zibbyv <zibby.v@gmail.com>
2024-10-21 06:57:16 +0000

    betty fixes

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Description: 'prints a phrase'
- * 
- * Return: Always 0 (Success)
- */
- 
+* main - Entry point
+*
+* Description: 'prints a phrase'
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
