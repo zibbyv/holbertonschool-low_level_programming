e876250d81fde4e876db54a040c33a79d46a8981
zibbyv <zibby.v@gmail.com>
2024-10-21 08:43:45 +0000

    C program that prints the size of various types on the computer it is compiled and run on

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'prints size of types'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
--- hello_world/README.md
+++ hello_world/README.md
@@ -4,3 +4,5 @@ C - Hello, World
 * 2-assembler: script that generates the assembly code of a C code and save it in an output file
 * 3-name: script that compiles a C file and creates an executable named cisfun
 * 4-puts.c: C program that prints exactly Programming is like building a multilingual puzzle
+* 5-printf: C program that prints exactly
+* 6-size: C program that prints the size of various types on the computer it is compiled and run on
