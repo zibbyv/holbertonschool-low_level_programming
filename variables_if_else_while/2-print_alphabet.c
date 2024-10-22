7732985d2e0251b62e64228988362821e9bfadea
zibbyv <zibby.v@gmail.com>
2024-10-22 05:10:01 +0000

    program that prints the alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,16 @@
 * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
 char a = 97;
 
-while (a <= 122) {
+while (a <= 122)
+{
 putchar(a);
 a++;
 }
 
 putchar('\n');
-return 0;
+return (0);
 }
