a13438268e2667402d1bceab131c921386118349
zibbyv <zibby.v@gmail.com>
2024-10-25 23:44:25 +0000

    checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _islower - checks if a character is lowercase
+* @c: checks for character
+* Return: 1 if lowercase, 0 otherwise
+*/
+int _isalpha(int c)
+
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
