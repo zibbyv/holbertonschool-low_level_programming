b6db87f280ea3b28317bdb65fd7b823c290f7721
zibbyv <zibby.v@gmail.com>
2024-10-25 09:08:51 +0000

    prints the alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,17 @@ void print_alphabet_x10(void)
 {
 int count = 1;
 
-while (count <= 10)
-	{
+	while (count <= 10)
+{
 	char letter = 'a';
 
-		while (letter <= 'z')
-		{
+	while (letter <= 'z')
+{
 	_putchar(letter);
 	letter++;
-		}
+}
 
 	_putchar('\n');
 	count++;
-	}
+}
 }
