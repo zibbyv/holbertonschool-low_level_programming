be1df148508f248724d9f0518a7cff5b27a72ac0
zibbyv <zibby.v@gmail.com>
2024-10-26 01:36:41 +0000

    prints the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* print_last_digit - prints the last digit of a number
+* @r: the number to check
+* Return: The value of r
+*/
+int print_last_digit(int r)
+{
+int n = r % 10;
+ if (n < 0)
+        n = -n;
+
+_putchar('0' + n);
+return (n);
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -6,3 +6,4 @@ C - Functions nested loops
 * 4-isalpha: checks for alphabetic character
 * 5-sign: checks if a number is positive zero or negative
 * 6-abs: computes the absolute value of an integer
+* 7-print_last_digit: prints the last digit of a number
