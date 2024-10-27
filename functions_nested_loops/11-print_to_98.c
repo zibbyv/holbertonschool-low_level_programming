3e94cc2afe3de9eea213ea2168028baacf41321d
zibbyv <zibby.v@gmail.com>
2024-10-27 05:28:13 +0000

    prints all natural numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,6 @@
 
 /**
 * print_to_98 - prints all natural numbers from n to 98
-*
 * @n: the starting number
 */
 void print_to_98(int n)
@@ -10,26 +9,31 @@ void print_to_98(int n)
 while (n != 98)
 {
 if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-
-if (n >= 10)
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-
-if (n != 98)
-{
+	{
+	_putchar('-');
+	if (n <= -100)
+	_putchar(((-n) / 100) + '0');
+	if (n <= -10)
+	_putchar(((-n / 10) % 10) + '0');
+	_putchar((-n % 10) + '0');
+	}
+	else
+		{
+		if (n >= 100)
+		_putchar((n / 100) + '0');
+		if (n >= 10)
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+			}
 _putchar(',');
 _putchar(' ');
-}
 
 if (n < 98)
 n++;
 else
 n--;
 }
+
 _putchar('9');
 _putchar('8');
 _putchar('\n');
