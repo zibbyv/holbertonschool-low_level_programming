ca0e44db55be25d81b8ab3f6dee564f45e81ca10
zibbyv <zibby.v@gmail.com>
2024-10-28 00:38:24 +0000

    checks if a character is a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* _isupper - checks if a character is a digit
+* _isdigit - checks if a character is a digit
 * @c: checks for character
 * Return: 1 if digit, 0 otherwise
 */
