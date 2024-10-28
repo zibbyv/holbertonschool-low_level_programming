b7b03092a08f78e010f7aeac5f529d7b9aa3f1f9
zibbyv <zibby.v@gmail.com>
2024-10-28 00:42:29 +0000

    multiplies two integers and returns the result

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiplies two integers and returns the result
+* @a: the first integer
+* @b: the second integer
+*
+* Return: the value of add n
+*/
+
+int mul(int a, int b);
+
+{
+	int n = a * b; /* calculate the sum of a multiplied by b */
+
+	return (n); /* return the value of mul */
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 C - More functions, more nested loops
 * 0-isupper: checks if a character is uppercase
 * 1-isdigit: checks if a character is a digit
+* 2-mul: multiplies two integers and returns the result
