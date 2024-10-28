018c13b921feca3c53f065f99769a1ad58d616fc
zibbyv <zibby.v@gmail.com>
2024-10-28 05:29:15 +0000

    print squares

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - print a square
+* @n: size of square
+*/
+
+void print_square(int size)
+
+{
+int y = 0, x;
+
+if (size <= 0)
+_putchar('\n');
+
+while (y < size)
+	{
+
+	x = 0;
+
+	while (x < size)
+	{
+	_putchar(35);
+	x++;
+	}
+	
+_putchar('\n');	
+y++;
+}
+}
