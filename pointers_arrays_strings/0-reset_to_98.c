6b839d9e04575cae603cff72d1eb1b0855cb57a1
zibbyv <zibby.v@gmail.com>
2024-10-29 02:35:59 +0000

    point to and update an int

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
-* print_line - point to and update an int
-* @*n: the int to point to
+* reset_to_98 - point to and update an int
+* @n: the int to point to
 */
 
 void reset_to_98(int *n)
