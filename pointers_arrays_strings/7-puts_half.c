c1a890e8a71176fe0450de4c23bafa7b0cf8b3f6
zibbyv <zibby.v@gmail.com>
2024-11-02 07:59:00 +0000

    update

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,21 +8,21 @@
 void puts_half(char *str)
 {
 int length = 0;
-int end;
+int start;
 int i;
 
 /* calculate the length of the string */
 while (str[length] != '\0')
 length++;
 
-/* ending point for printing the first half */
+/* starting point for printing the second half */
 if (length % 2 == 0)
-end = length / 2; /* even length end halfway */
+start = length / 2; /* even length start halfway */
 else
-end = (length + 1) / 2; /* odd length round up for halfway */
+start = (length + 1) / 2; /* odd length round up for halfway */
 
 /* print the first half of the string */
-for (i = 0; i < end; i++)
+for (i = start; i < length; i++)
 {
 _putchar(str[i]);
 }
