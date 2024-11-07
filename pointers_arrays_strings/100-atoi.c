ea0bd245d2e388ac363e0f5eceb039c4c0f46504
zibbyv <zibby.v@gmail.com>
2024-11-07 23:02:21 +0000

    converts a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,40 +2,40 @@
 #include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- *
- * Return: the integer value
- */
+* _atoi - converts a string to an integer
+* @s: the string to be converted
+*
+* Return: the integer value
+*/
 
 int _atoi(char *s)
-{
-    int count = 0;
-    int sign = 1;
-    unsigned int finalnum = 0;
-    int digit;
-    int evalnum = 0;
+	{
+	int count = 0;
+	int sign = 1;
+	unsigned int finalnum = 0;
+	int digit;
+	int evalnum = 0;
 
-    while (s[count] != '\0')
-    {
-        digit = s[count];
+	while (s[count] != '\0')
+	{
+	digit = s[count];
 
-        /* Check if character is a digit */
-        if (digit >= '0' && digit <= '9')
-        {
-            evalnum = 1; /* Mark start reading a number */
-            finalnum = (finalnum * 10) + (digit - '0');
-        }
-        else if (evalnum)
-        {
-            break;
-        }
-        else if (s[count] == '-') /* handle sign */
-        {
-            sign *= -1;
-        }
-        count++;
-    }
+		/* Check if character is a digit */
+		if (digit >= '0' && digit <= '9')
+		{
+			evalnum = 1; /* Mark start reading a number */
+			finalnum = (finalnum * 10) + (digit - '0');
+		}
+		else if (evalnum)
+		{
+			break;
+		}
+		else if (s[count] == '-') /* handle sign */
+		{
+		sign *= -1;
+			}
+			count++;
+			}
 
-    return (sign * finalnum);
+	return (sign * finalnum);
 }
