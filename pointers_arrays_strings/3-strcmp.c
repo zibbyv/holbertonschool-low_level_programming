d8c7da24c104ce5d4aaaee34e7156d5d73dbf753
zibbyv <zibby.v@gmail.com>
2024-11-08 02:38:25 +0000

    compares strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcmp - compare two strings
+* @s1: first string
+* @s2: second string
+* Return: the difference
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+/* traverse string */
+while (s1[i] != '\0')
+{
+	if (s1[i] != s2[i])
+	{
+	return (s1[i] - s2[i]); /* return the difference */
+}
+i++;
+}
+return (s1[i] - s2[i]);
+}
