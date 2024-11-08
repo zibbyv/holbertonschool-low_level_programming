d5d51f2909c24fa516cc8bce52efdac61baeccd9
zibbyv <zibby.v@gmail.com>
2024-11-08 02:22:20 +0000

    copies up to n characters from the src string to the dest string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 
 /**
-* _strncat - appends up to n characters from src string to the dest string
+ * _strncpy - copies up to n characters from the src string to the dest string
 * @dest: destination string
 * @src: source string
-* @n: maximum number of characters to append from src
+* @n: maximum number of characters to copy
 * Return: the destination value
 */
-
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-char *p = dest;
+int i = 0;
 
-/* Move p to the end of dest */
-while (*p)
-p++;
-
-/* Append up to n characters from src to dest */
-while (n > 0 && *src != '\0') {
-*p++ = *src++;
-n--;
+/* Copy characters from src to dest until n characters or the end of src */
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
 }
 
-/* Add the null terminator at the end */
-*p = '\0';
+/* If there are fewer than n characters in src pad dest with '\0' */
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 
 return (dest);
 }
