d83760bc00d8f9256ef2cc4d3cd9bc0918ae516c
zibbyv <zibby.v@gmail.com>
2024-11-17 04:02:35 +0000

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) {
+/**
+ * main - prints the name of the program
+ * @argc: number of command-line arguments
+ * @argv: array of strings representing the command-line arguments
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
 (void)argc;  /* Suppress the unused parameter warning */
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
