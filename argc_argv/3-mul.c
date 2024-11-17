ec2e573880a1c00c6017299ad4c7dc61581cffd5
zibbyv <zibby.v@gmail.com>
2024-11-17 05:35:28 +0000

    program that prints all arguments it receives

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 int num1, num2, result;
 
-if (argc != 3)  /* Check if there are exactly 3 arguments (program name + 2 numbers) */
+if (argc != 3)  /* Check if there are exactly 3 arguments */
 {
 printf("Error\n");
 return (1);
@@ -20,7 +20,7 @@ return (1);
 
 num1 = atoi(argv[1]);  /* Convert the first argument to an integer */
 num2 = atoi(argv[2]);  /* Convert the second argument to an integer */
-result = num1 * num2;  /* Multiply the two numbers */
+result = num1 *num2;  /* Multiply the two numbers */
 
 printf("%d\n", result);  /* Print the result followed by a new line */
 
